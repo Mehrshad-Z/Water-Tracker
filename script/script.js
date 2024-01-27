02355c57b970bf05df56bc4717a1e80fe6bc7338
Mehrshad Zarifian <work.mehrshadzarifian@gmail.com>
2024-01-27 22:22:18 +0330

    Add script for water tracking

---
+++ script/script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const addBtn = document.querySelector(".plus");
+const setBtn = document.querySelector(".set");
+let selectDish = document.querySelectorAll(".dish");
+let displayDrinkedWater = document.querySelector(".value-drinked-water");
+let displayTarget = document.querySelector(".value-target");
+let displayDetailAdd = document.querySelector(".details-plus");
+
+let drinkedWater = 0;
+displayDrinkedWater.textContent = drinkedWater;
+let dish = 250 ;
+displayDetailAdd.textContent = `${dish} ml`;
+let valueTarget = 3200;
+displayTarget.textContent = valueTarget;
+
+for (let i = 0; i < selectDish.length; i++) {
+  selectDish[i].addEventListener("click", function () {
+    dish = selectDish[i].children[0].textContent;
+    displayDetailAdd.textContent = `${dish} ml`;
+    return dish;
+  });
+}
+
+setBtn.addEventListener("click", function () {
+  valueTarget = Number(document.getElementById("value-target").value);
+  displayTarget.textContent = valueTarget;
+  return valueTarget;
+});
+
+addBtn.addEventListener("click", function () {
+  if (drinkedWater < valueTarget) {
+    drinkedWater += Number(dish);
+    displayDrinkedWater.textContent = drinkedWater;
+  } else if (drinkedWater == valueTarget) {
+    displayDrinkedWater.textContent = valueTarget;
+    addBtn.classList.add("hidden");
+    }
+});
